d22ca8384e524e62549703068c0343a35d341525
rahmadahyaWeb <thelastpc24@gmail.com>
2026-03-17 13:27:40 +0800

    fix sidebar layout

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -7,35 +7,45 @@
 
 <body class="min-h-screen bg-white dark:bg-zinc-800">
     <flux:sidebar sticky collapsible="mobile"
-        class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+        class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900 h-screen flex flex-col">
+
+        {{-- HEADER (fixed height) --}}
         <flux:sidebar.header>
             <x-app-logo :sidebar="true" href="{{ route('dashboard') }}" wire:navigate />
             <flux:sidebar.collapse class="lg:hidden" />
         </flux:sidebar.header>
 
-        <flux:sidebar.nav>
+        {{-- MAIN NAV (FULL HEIGHT + SCROLLABLE) --}}
+        <div class="flex-1 flex flex-col overflow-hidden">
 
-            @foreach (config('sidebar') as $group)
-                <x-sidebar-item :label="$group['label']" :icon="$group['icon'] ?? 'circle'" :route="$group['route'] ?? null" :permission="$group['permission'] ?? null"
-                    :role="$group['role'] ?? null" :children="$group['children'] ?? []" />
-            @endforeach
+            {{-- MENU LIST --}}
+            <flux:sidebar.nav class="flex-1 overflow-y-auto">
+                @foreach (config('sidebar') as $group)
+                    <x-sidebar-item :label="$group['label']" :icon="$group['icon'] ?? 'circle'" :route="$group['route'] ?? null" :permission="$group['permission'] ?? null"
+                        :role="$group['role'] ?? null" :children="$group['children'] ?? []" />
+                @endforeach
+            </flux:sidebar.nav>
 
-        </flux:sidebar.nav>
+            {{-- STATIC BOTTOM MENU --}}
+            {{-- <flux:sidebar.nav class="border-t border-zinc-200 dark:border-zinc-700">
+                <flux:sidebar.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit"
+                    target="_blank">
+                    {{ __('Repository') }}
+                </flux:sidebar.item>
 
-        <flux:spacer />
+                <flux:sidebar.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire"
+                    target="_blank">
+                    {{ __('Documentation') }}
+                </flux:sidebar.item>
+            </flux:sidebar.nav> --}}
 
-        <flux:sidebar.nav>
-            <flux:sidebar.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-            </flux:sidebar.item>
+        </div>
 
-            <flux:sidebar.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire"
-                target="_blank">
-                {{ __('Documentation') }}
-            </flux:sidebar.item>
-        </flux:sidebar.nav>
+        {{-- USER MENU (FIXED BOTTOM, NOT SCROLLING) --}}
+        <div class="sticky bottom-0 bg-zinc-50 dark:bg-zinc-900 z-10">
+            <x-desktop-user-menu class="hidden lg:block" :name="auth()->user()->name" />
+        </div>
 
-        <x-desktop-user-menu class="hidden lg:block" :name="auth()->user()->name" />
     </flux:sidebar>
 
     <!-- Mobile User Menu -->
