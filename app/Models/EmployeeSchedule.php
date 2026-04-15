b4c9f83a06a3487431c126a340587546e8572a51
rahmadahyaWeb <thelastpc24@gmail.com>
2026-04-15 09:21:51 +0800

    fix

--- app/Models/EmployeeSchedule.php
+++ app/Models/EmployeeSchedule.php
@@ -14,6 +14,10 @@ class EmployeeSchedule extends Model
         'is_active',
     ];
 
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
